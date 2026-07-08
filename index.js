53e0416d02b25f476e3cea290449ff120bc3c5e4
suoyukii <heydon-lee@outlook.com>
2026-07-08 20:31:31 +0800

    Optimize code and add README.md

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
+function error(tip) {
+  const dialog = document.querySelector("dialog");
+  dialog.innerText = tip;
+  dialog.show();
+  setTimeout(() => dialog.close(), 2000);
+}
+
 function get_link() {
-  let input = document.querySelector("input");
-  let url = input.value;
+  let url = document.querySelector("input").value;
   if (/song\?id=\d+/.test(url)) {
     url = url.match(/id=(\d+)/)[1];
   } else if (/playlist\?id=\d+/.test(url)) {
-    return error(input, "不支持 歌曲列表链接");
+    error("不支持 歌曲列表链接");
+    return;
   } else if (/program\?id=\d+/.test(url)) {
-    return error(input, "不支持 电台链接");
+    error("不支持 电台链接");
+    return;
   } else if (/album\/\d+/.test(url)) {
-    return error(input, "不支持 专辑链接");
+    error("不支持 专辑链接");
+    return;
   } else {
-    if (!/^\d+$/.test(url))
-      return error(input, "请输入 网易云音乐的链接 / 歌曲ID");
+    if (!/^\d+$/.test(url)) {
+      error("请输入 网易云音乐的链接 / 歌曲ID");
+      return;
+    }
   }
   window.open(`https://music.163.com/song/media/outer/url?id=${url}.mp3`);
 }
-function error(input, tip) {
-  input.value = "";
-  input.placeholder = tip;
-}
 
-document.querySelector("button").addEventListener("click", get_link);
-document.addEventListener("keydown", (e) => {
+function enter_key(e) {
   if (e.key === "Enter") get_link();
-});
+}
